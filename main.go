a023f95f1116529b31b83dc1f16954c345892eaa
nmirallegro <mirallegron@gmail.com>
2025-03-08 17:37:00 -0500

    added staticcheck

--- main.go
+++ main.go
@@ -24,6 +24,11 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
+func unused() {
+    // this function does nothing
+    // and is called nowhere
+}
+
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
@@ -95,4 +100,5 @@ func main() {
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
+	
 }
